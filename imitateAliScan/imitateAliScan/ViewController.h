ca1fd7e09b3e86872b660d72777489eb7ed2e74b
zhangli <313890735@qq.com>
2016-02-24 15:46:44 +0800

    初始化项目

---
+++ imitateAliScan/imitateAliScan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  imitateAliScan
+//
+//  Created by 余亮 on 16/2/24.
+//  Copyright © 2016年 余亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
